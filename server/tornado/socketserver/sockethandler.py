8f27521055af98a4f9c8f333cc725f21375c1f5f
liuwel <liuwel@live.com>
2016-11-16 17:34:58 +0800

    modify message event

--- server/tornado/socketserver/sockethandler.py
+++ server/tornado/socketserver/sockethandler.py
@@ -1,6 +1,8 @@
-#coding:utf-8
+# coding:utf-8
 import tornado.websocket
 import json
+import time
+
 class SocketHandler(tornado.websocket.WebSocketHandler):
     def check_origin(self, origin):
         return True
@@ -9,18 +11,23 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
 
     @staticmethod
     def send_all(message):
-        for c in SocketHandler.clients :
-            c.write_message(json.dumps(message))
+        for c in SocketHandler.clients:
+            c.write_message(message)
 
     def open(self):
-        self.write_message(u'=====open=====')
+        # self.write_message(u'=====open=====')
         SocketHandler.clients.add(self)
-        print(id(self))
-        print(SocketHandler.clients)
-        print("WebSocket opened")
+        # print(id(self))
+        # print(SocketHandler.clients)
+        # print("WebSocket opened")
 
     def on_message(self, message):
-        SocketHandler.send_all(message)
+        msg = json.loads(message)
+        if msg['command'] == 'message':
+            reMsg = {'command':'message','options':{'type':1,'content':msg['options']['content'],'time':time.time()}}
+            SocketHandler.send_all(json.dumps(reMsg))
+        elif msg['command'] == 'init':
+            pass
 
     def on_close(self):
         SocketHandler.clients.remove(self)
